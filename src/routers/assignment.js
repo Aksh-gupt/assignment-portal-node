750ce5a4837eddd2e040e7acae11712d71a1be2e
unknown <akshatgupta486@gmail.com>
2020-12-05 12:38:23 +0530

    after add assignment router

--- src/routers/assignment.js
+++ src/routers/assignment.js
@@ -6,38 +6,36 @@ const authTeacher = require("../middleware/authTeacher")
 
 
 const upload = multer({
-    dest: "files",
     limits: {
         fileSize: 5000000
-    },
-    fileFilter(req,file,cb){
-        if(!file.originalname.endsWith(".pdf")){
-            throw new Error("Please upload a pdf")
-        }
-
-        cb(undefined,true)
     }
+    // fileFilter(req,file,cb){
+    //     if(!file.originalname.endsWith(".pdf")){
+    //         throw new Error("Please upload a pdf")
+    //     }
+
+    //     cb(undefined,true)
+    // }
 })
 
-router.post("/assignment/make",authTeacher,upload.single('document') ,async (req,res) => {
-    // const assignment = new Assignment({
-    //     document: req.file.buffer,
-    //     name: req.body.name,
-    //     bss: req.body.bss,
-    //     subid: req.body.subid,
-    //     last: req.body.last,
-    //     description: req.body.description,
-    //     owner: req.teacher._id
-    // })
-    
+var cpUpload = upload.fields([{ name: 'document', maxCount: 1 }, { name: 'overrides', maxCount: 1 }])
+router.post("/assignment/make",authTeacher,cpUpload ,async (req,res) => {
     try{
-        // console.log(assignment);
-        // await assignment.save()
-        console.log(req.body)
-        console.log(req.file)
-        res.status(201).send()
+        const val = JSON.parse(req.files['overrides'][0].buffer.toString())
+        const assignment = new Assignment({
+            document: req.files['document'][0].buffer,
+            name: val.name,
+            bss: val.bss,
+            subid: val.subid,
+            last: val.last,
+            description: val.description,
+            owner: req.teacher._id
+        })
+        // console.log(assignment)
+        await assignment.save()
+        res.status(201).send({_id: assignment._id, name: assignment.name})
     }catch(e){
-        res.status(400).send(e)
+        res.status(400).send({error: e.message})
     }
 })
 
@@ -65,14 +63,23 @@ router.patch("/updateassignment/:id",authTeacher , async(req,res) => {
     }
 })
 
-router.get("/allassignment",authTeacher ,async(req,res) => {
+router.post("/allassignment",authTeacher ,async(req,res) => {
     try{
-        // const code = await Code.find({owner: req.user._id});
-        await req.teacher.populate('assignments').execPopulate()
+        const assignments = await Assignment.find({owner: req.teacher._id, subid: req.body.subid, bss: req.body.bss});
         // if(!code){
         //     res.status(404).send()
         // }
-        res.send(req.teacher.assignments)
+        const assign = assignments.map((assignment) => {
+            const temp = assignment.toObject();
+            delete temp.bss;
+            delete temp.document;
+            delete temp.description;
+            delete temp.last;
+            delete temp.subid;
+            delete temp.owner;
+            return temp
+        })
+        res.status(200).send(assign)
     }catch(e){
         res.status(500).send()
     }
