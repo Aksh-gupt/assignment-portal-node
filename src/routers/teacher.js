3a07fcc2804c2b34796d8a665dbf0551c90580b4
Aksh-gupt <37631955+Aksh-gupt@users.noreply.github.com>
2020-12-07 01:12:37 +0530

    Update teacher.js

--- src/routers/teacher.js
+++ src/routers/teacher.js
@@ -32,26 +32,6 @@ router.post("/teacher/login", async(req, res) => {
     }
 })
 
-router.post("/teacher/addsubject",authTeacher,async(req,res) => {
-    try{
-        const subject = await Subject.findOne({name: req.body.subject})
-        req.teacher.teach = req.teacher.teach.concat({bsss: req.body.bsss, subject: req.body.subject, subid: subject.subid});
-        await req.teacher.save();
-        var len = req.teacher.teach.length
-        res.status(200).send(req.teacher.teach[len-1]);
-    }catch(e){
-        // console.log(e)
-        res.status(400).send(e);
-    }
-})
-
-router.get("/teacher/mysubject",authTeacher,async(req,res) => {
-    try{
-        res.status(200).send(req.teacher.teach)
-    }catch(e){
-        res.status(400).send()
-    }
-})
 
 router.get("/allteacher",async(req,res) => {
     try{
